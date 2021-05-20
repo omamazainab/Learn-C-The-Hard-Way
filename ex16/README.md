40772c4df114581f34e0bda62af467791b7fc228
omama zainab <omamazainab00@gmail.com>
2021-05-20 23:51:24 +0500

    chore: ex16 extra credit

--- ex16/README.md
+++ ex16/README.md
@@ -495,3 +495,90 @@ Weight: 190
 ==42341== ERROR SUMMARY: 1 errors from 1 contexts (suppressed: 0 from 0)
 Segmentation fault (core dumped)
 ```
+
+## Extra Credit
+
+### How to create a struct on the stack, just like you’re making any other variable.
+### How to initialize it using the x.y (period) character instead of the x->y syntax.
+### How to pass a structure to other functions without using a pointer.
+
+```c
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Person
+{
+    char *name;
+    int age;
+    int height;
+    int weight;
+};
+
+struct Person Person_create(char *name, int age, int height, int weight)
+{
+    struct Person who;
+
+    who.name = strdup(name);
+    who.age = age;
+    who.height = height;
+    who.weight = weight;
+
+    return who;
+}
+
+void person_print(struct Person who){
+    printf("Name: %s \n",who.name);
+    printf("Age: %d \n",who.age);
+    printf("Height: %d \n",who.height);
+    printf("Weight: %d \n",who.weight);
+}
+
+int main(int argc, char *argv[])
+{
+
+    struct Person Joe = Person_create("Joe",30,120,150);
+    struct Person Frank = Person_create("Frank",24,130,140);
+
+    person_print(Joe);
+
+    person_print(Frank);
+
+    Joe.age += 20;
+    Joe.height -= 2;
+    Joe.weight +=40;
+
+    person_print(Joe);
+
+    Frank.age += 20;
+    Frank.weight += 20;
+
+    person_print(Frank);
+
+
+    return 0;
+}
+
+```
+
+```
+$ ./ex16
+Name: Joe 
+Age: 30 
+Height: 120 
+Weight: 150 
+Name: Frank 
+Age: 24 
+Height: 130 
+Weight: 140 
+Name: Joe 
+Age: 50 
+Height: 118 
+Weight: 190 
+Name: Frank 
+Age: 44 
+Height: 130 
+Weight: 160 
+
+```
--- ex16/ex16.c
+++ ex16/ex16.c
@@ -11,129 +11,46 @@ struct Person
     int weight;
 };
 
-struct Person *Person_create(char *name, int age, int height, int weight)
+struct Person Person_create(char *name, int age, int height, int weight)
 {
-    struct Person *who = malloc(sizeof(struct Person));
+    struct Person who;
 
-    assert(who != NULL);
-
-    who->name = strdup(name);
-    who->age = age;
-    who->height = height;
-    who->weight = weight;
+    who.name = strdup(name);
+    who.age = age;
+    who.height = height;
+    who.weight = weight;
 
     return who;
 }
 
-void person_destroy(struct Person *who){
-    assert(who != NULL);
-
-    free(who->name);
-    free(who);
-}
-
-void person_print(struct Person *who){
-    printf("Name: %s \n",who->name);
-    printf("Age: %d \n",who->age);
-    printf("Height: %d \n",who->height);
-    printf("Weight: %d \n",who->weight);
+void person_print(struct Person who){
+    printf("Name: %s \n",who.name);
+    printf("Age: %d \n",who.age);
+    printf("Height: %d \n",who.height);
+    printf("Weight: %d \n",who.weight);
 }
 
 int main(int argc, char *argv[])
 {
 
-    struct Person *Joe = Person_create("Joe",30,120,150);
-    struct Person *Frank = Person_create("Frank",24,130,140);
+    struct Person Joe = Person_create("Joe",30,120,150);
+    struct Person Frank = Person_create("Frank",24,130,140);
 
-    printf("Joe at position : %p ",Joe);
     person_print(Joe);
 
-    printf("Frank at position : %p ",Frank);
     person_print(Frank);
 
-    Joe->age += 20;
-    Joe->height -= 2;
-    Joe->weight +=40;
+    Joe.age += 20;
+    Joe.height -= 2;
+    Joe.weight +=40;
 
     person_print(Joe);
 
-    Frank->age += 20;
-    Frank->weight += 20;
+    Frank.age += 20;
+    Frank.weight += 20;
 
-    person_print(NULL);
+    person_print(Frank);
 
-    person_destroy(Joe);
-    person_destroy(Frank);
 
     return 0;
 }
-
-// #include <stdio.h>
-// #include <assert.h>
-// #include <stdlib.h>
-// #include <string.h>
-
-// struct Person
-// {
-//     char *name;
-//     int age;
-//     int height;
-//     int weight;
-// };
-
-// struct Person *Person_create(char *name, int age, int height, int weight)
-// {
-//     struct Person *who = malloc(sizeof(struct Person));
-
-//     assert(who != NULL);
-
-//     who->name = strdup(name);
-//     who->age = age;
-//     who->height = height;
-//     who->weight = weight;
-
-//     return who;
-// }
-
-// void person_destroy(struct Person *who){
-//     assert(who != NULL);
-
-//     free(who->name);
-//     free(who);
-// }
-
-// void person_print(struct Person *who){
-//     printf("Name: %s \n",who->name);
-//     printf("Age: %d \n",who->age);
-//     printf("Height: %d \n",who->height);
-//     printf("Weight: %d \n",who->weight);
-// }
-
-// int main(int argc, char *argv[])
-// {
-
-//     struct Person *Joe = Person_create("Joe",30,120,150);
-//     struct Person *Frank = Person_create("Frank",24,130,140);
-
-//     printf("Joe at position : %p ",Joe);
-//     person_print(Joe);
-
-//     printf("Frank at position : %p ",Frank);
-//     person_print(Frank);
-
-//     Joe->age += 20;
-//     Joe->height -= 2;
-//     Joe->weight +=40;
-
-//     person_print(Joe);
-
-//     Frank->age += 20;
-//     Frank->weight += 20;
-
-//     person_print(Frank);
-
-//     person_destroy(Joe);
-//     person_destroy(Frank);
-
-//     return 0;
-// }
